66010819c82149d21938d559ab12b07d197a5c09
Brian <riceb53@gmail.com>
2018-03-06 20:47:00 -0600

    make route for all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+  def every_product
+    # grab the data from the db
+    products = Product.all
+    # show the data to to the user
+    render json: products.as_json
+  end
 end
